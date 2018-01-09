481b0e2b28cb18d5f188ad3624649aef7f5dafbe
Mattias Buelens <mattias@buelens.com>
2018-01-10 00:20:02 +0100

    Output ES bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,12 @@ const pkg = require('./package.json');
 
 export default {
 	input: 'src/vlq.js',
-	output: {
+	output: [{
 		file: pkg.main,
 		format: 'umd',
 		name: 'vlq'
-	}
+	}, {
+		file: pkg.module,
+		format: 'es'
+	}]
 };
